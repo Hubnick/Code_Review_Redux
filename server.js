bdcd6d4cd0cb088036902c21315c92713d6dc1e8
Hubnick <jacoby.nick@gmail.com>
2018-10-11 12:36:05 -0400

    handled all bars

--- server.js
+++ server.js
@@ -122,12 +122,12 @@ var cards = [
 
 
 // Routes
-app.get("/", function (req, res) {
+app.get("/categories", function (req, res) {
   data = cards;
   res.render("categorIES", data);
 });
 
-app.get("/cat", function (req, res) {
+app.get("/review", function (req, res) {
   data = cards;
   res.render("categorY", data);
 });
@@ -152,15 +152,15 @@ app.get("/all-cards", function (req, res) {
 
 
 
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
 
 
-db.sequelize.sync({ force: true }).then(function () {
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+// db.sequelize.sync({ force: true }).then(function () {
+//   app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
 
 //potential CONFLICT between above and below LISTEN
 
