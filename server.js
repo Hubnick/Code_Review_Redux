e7dad6aa908a9954f76e9778b0f3263fd0e568bf
embilombo <emreerendc@gmail.com>
2018-10-11 11:50:31 -0400

    updated some sections

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 
+//Dependencies
 var express = require("express");
+var bodyParser=require("body-parser");
+var session=require("express-session");
+var passport=require(".config/passport");
 
 
-
-var app = express();
 var PORT = process.env.PORT || 8080;
-
 var db = require("./models");
 
+//Setting Express
+var app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json);
 app.use(express.static("public"));
+app.use(session({secret:"codereview", resave: true, saveUninitialzed:true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync.then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
