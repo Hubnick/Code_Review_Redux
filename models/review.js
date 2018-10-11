076edaf1d70d3988ef482c1e5ac2f9b05dedc810
embilombo <emreerendc@gmail.com>
2018-10-11 13:26:35 -0400

    passport.js

--- models/review.js
+++ models/review.js
@@ -23,25 +23,6 @@ module.exports = function(sequelize, DataTypes) {
   return User;
 };
 
-// module.exports = function(sequelize, DataKinds) {
-// var User = sequelize.define("User", {
-//     cards: {
-//         //figure out how to properly write id
-//         id: DataKinds.STRING,
-//         username: DataKinds.STRING,
-//         password: DataKinds.STRING,
-//         category1: DataKinds.BOOLEAN,
-//         category2: DataKinds.BOOLEAN,
-//         category3: DataKinds.BOOLEAN,
-//         category4: DataKinds.BOOLEAN,
-//         category5: DataKinds.BOOLEAN,
-//         category6: DataKinds.BOOLEAN
-
-//     }
-// });
-// return User;
-// };
-
 
 
 
