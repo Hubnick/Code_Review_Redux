9f6a983ccbc1b61901516bbb0a01936681170717
Sara Anspach <sara.anspach@gmail.com>
2018-10-12 11:21:30 -0400

    first

--- public/assets/js/animate.js
+++ public/assets/js/animate.js
@@ -3,14 +3,33 @@ $(document).ready(function() {
 
 
   $("#testingContainer").flip({
-  trigger: 'click',
-  front: '.question',
-  back: '.answer',
-  speed: '400'
+  trigger: "click",
+  front: ".question",
+  back: ".answer",
+  speed: "400"
+});
+
 });
 
 
+$(document).ready(function(){
+  $("#module-html").click(function(){
+    animate();
+  });
+  $("#module-css").click(function(){
+    animate();
+  });
+  $("#module-jquery").click(function(){
+    animate();
+  });
+  $("#module-javascript").click(function(){
+    animate();
+  });
+  $("#module-api").click(function(){
+    animate();
+  });
+  $("#module-git").click(function(){
+    animate();
+  });
 });
-
-
 
