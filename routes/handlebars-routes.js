0726d9ca2851533070b55d2b319370a362e2d3d0
Hubnick <jacoby.nick@gmail.com>
2018-10-14 19:26:43 -0400

    92 percent

--- routes/handlebars-routes.js
+++ routes/handlebars-routes.js
@@ -25,64 +25,68 @@ module.exports = function (app) {
             },
 
 
-                // app.get("/signup", function (req, res) {
-
-                //     res.render("signup");
-                // },
-                // app.get("/sets", function (req, res) {
-                //  
-                //   res.render("sets");
-                // },
-
-                // app.get("/cards", function (req, res) {
-                //  
-                //   res.render("cards");
-                // },
-
-                app.get("/module-html", function (req, res) {
-
-                    res.render("module-html");
+                app.get("/signup", function (req, res) {
+                    if (req.user) {
+                        res.redirect("/members");
+                    }
+                    res.render("signup");
                 },
 
-                    app.get("/module-css", function (req, res) {
+                    // app.get("/sets", function (req, res) {
+                    //  
+                    //   res.render("sets");
+                    // },
 
-                        res.render("module-css");
+                    // app.get("/cards", function (req, res) {
+                    //  
+                    //   res.render("cards");
+                    // },
+
+                    app.get("/module-html", function (req, res) {
+
+                        res.render("module-html");
                     },
 
-                        app.get("/module-javascript", function (req, res) {
+                        app.get("/module-css", function (req, res) {
 
-                            res.render("module-javascript");
+                            res.render("module-css");
                         },
 
-                            app.get("/module-jquery", function (req, res) {
+                            app.get("/module-javascript", function (req, res) {
 
-                                res.render("module-jquery");
+                                res.render("module-javascript");
                             },
 
-                                app.get("/module-api", function (req, res) {
+                                app.get("/module-jquery", function (req, res) {
 
-                                    res.render("module-api");
+                                    res.render("module-jquery");
                                 },
 
-                                    app.get("/module-git", function (req, res) {
+                                    app.get("/module-api", function (req, res) {
 
-                                        res.render("module-git");
+                                        res.render("module-api");
                                     },
 
-                                        // app.get("/all-cards", function (req, res) {
-                                        //   // 2. Loop through the animals, and send those that are pets to the index handlebars file.
-                                        //   var data = {
-                                        //     cards: []
-                                        //   };
-                                        //   for (var i = 0; i < cards.length; i += 1) {
-                                        //     // Get the current animal.
-                                        //     var currentCard = cards[i];
-                                        //     data.cards.push(currentCard);
-                                        //   };
+                                        app.get("/module-git", function (req, res) {
 
-                                        //   res.render("index2");
-                                        // })
+                                            res.render("module-git");
+                                        },
 
+                                            // app.get("/all-cards", function (req, res) {
+                                            //   // 2. Loop through the animals, and send those that are pets to the index handlebars file.
+                                            //   var data = {
+                                            //     cards: []
+                                            //   };
+                                            //   for (var i = 0; i < cards.length; i += 1) {
+                                            //     // Get the current animal.
+                                            //     var currentCard = cards[i];
+                                            //     data.cards.push(currentCard);
+                                            //   };
+
+                                            //   res.render("index2");
+                                            // })
+
+                                        )
                                     )
                                 )
                             )
