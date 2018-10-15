6a0fd80e023c2713636a88e9e14d18471c0f6ab4
Hubnick <jacoby.nick@gmail.com>
2018-10-15 11:24:01 -0400

    WOOYEAAAAAAAAA

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,24 @@
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("MrWest","Kanye", "West", "kanyewest@aol.com", "000000");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("MrWest","Kanye", "West", "kanyewest@aol.com", "000000");
 
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("Crazienyc","Kramer", "Crazie", "kramer1@hotmail.com", "kingforever#");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("Crazienyc","Kramer", "Crazie", "kramer1@hotmail.com", "kingforever#");
 
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("BeautyDC","Bridget", "Pricella", "bpdc@yahoo.com", "Bpdc1999*");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("BeautyDC","Bridget", "Pricella", "bpdc@yahoo.com", "Bpdc1999*");
 
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("Wildwest","Shaun", "Baracuda", "thanx@gmail.com","bobo1976@");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("Wildwest","Shaun", "Baracuda", "thanx@gmail.com","bobo1976@");
 
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("Spiderman","Tatum", "Braddock", "flower24@gmail.com","yesabd999!");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("Spiderman","Tatum", "Braddock", "flower24@gmail.com","yesabd999!");
 
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("Player","Christiano", "Ronaldo", "ronaldo@king.com", "king2018$");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("Player","Christiano", "Ronaldo", "ronaldo@king.com", "king2018$");
 
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("Boxer","Mohammed", "Ali", "goldenwrist@forever.com", "legendforever*");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("Boxer","Mohammed", "Ali", "goldenwrist@forever.com", "legendforever*");
 
-INSERT INTO users (user_name,first_name,last_name,email,password)
-VALUES ("TATATA","Tracy", "Kogan", "tkogan@gmail.com", "Tcogan1989&");
+-- INSERT INTO users (user_name,first_name,last_name,email,password)
+-- VALUES ("TATATA","Tracy", "Kogan", "tkogan@gmail.com", "Tcogan1989&");
 
